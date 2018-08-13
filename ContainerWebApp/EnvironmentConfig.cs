4cd67d607b1c2ac529daf23d09d6f5dd0351578e
Clare Moore <cmoore@ermpower.com.au>
2018-08-13 16:21:41 +1000

    Playing with configuration
    Playing with configuration

---
+++ ContainerWebApp/EnvironmentConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContainerWebApp
+{
+    public class EnvironmentConfig
+    {
+        public string  ProfileName{ get; set; }
+    }
+}
--- ContainerWebApp/Startup.cs
+++ ContainerWebApp/Startup.cs
@@ -22,6 +22,7 @@ namespace ContainerWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.Configure<EnvironmentConfig>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
